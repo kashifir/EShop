65118c8f5383e508a937830b6ae0317f9be3089c
kashif <irshadkashifi.r@gmail.com>
2020-11-10 11:50:18 +0100

    update produit.vue

--- src/views/produit.vue
+++ src/views/produit.vue
@@ -1,33 +1,32 @@
 <template>
-    <div>
-        <Myproduit :produits="produits"/>
-    </div>
+<div>
+    <Myproduit :produits="produits" />
+</div>
 </template>
 
 <script>
-    import Myproduit from "../components/myproduit";
+import Myproduit from "../components/myproduit";
 
-
-    export default {
-        name: "produit",
-        data(){
-            return{
-                produits:{}
-            }
-        },
-        components:{
-            Myproduit
-        },
-        created() {
-            this.axios.get("http://localhost:3000/produit/all")
-                .then(res => {
-                    this.produits = res.data.produit
-                })
-                .catch(err => {
-                    alert(err)
-                })
+export default {
+    name: "produit",
+    data() {
+        return {
+            produits: {}
         }
+    },
+    components: {
+        Myproduit
+    },
+    created() {
+        this.axios.get("http://146.59.156.234:3000/produit/all")
+            .then(res => {
+                this.produits = res.data.produit
+            })
+            .catch(err => {
+                alert(err)
+            })
     }
+}
 </script>
 
 <style scoped>
